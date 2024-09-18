28f5ff416411b79ab57603ad47648e82ae0813af
Marcus <x@x.de>
2024-09-18 13:15:29 +0200

    erstes Setup

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "2.1.0"
+    }
+  }
+}
+
+provider "openstack" {
+  # Configuration options
+}
